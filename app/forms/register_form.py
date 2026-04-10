ab30ed75ad87693415395c7028e69a43d6bfaa2b
NotRealSilver <notrealsilver@yandex.com>
2026-04-10 21:15:40 +0500

    migration and base.html rework

--- app/forms/register_form.py
+++ app/forms/register_form.py
@@ -3,22 +3,9 @@ from wtforms import StringField, PasswordField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 
-class LoginForm(FlaskForm):
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    submit = SubmitField('Войти')
-
-
 class RegisterForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()])
     email = EmailField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     conf_password = PasswordField('Повторите пароль', validators=[DataRequired()])
     submit = SubmitField('Зарегистрироваться')
-
-
-class AccountSettings(FlaskForm):
-    username = StringField('Введите имя')
-    email = EmailField('Введите почту')
-    password = PasswordField('Введите пароль')
-    submit = SubmitField('Сохранить')
